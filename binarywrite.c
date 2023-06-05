1a2402e7870959d16b35cc95a547f6dcc9c4f773
qkqkwl159 <qkqkwl147@gmail.com>
2023-06-05 21:36:03 +0900

    0604

--- binarywrite.c
+++ binarywrite.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _User{
+		char id[255];
+		char age[255];
+		char HP[255];
+		double xp, yp;
+		char* friend[255];
+}User;
 
 int main() {
-    FILE* file;
-    char input[100];
+    FILE* file; 
+    char input[100] = "TEST";
+		int menu = 0;
     
     // 파일 열기
-    file = fopen("userdata.bin", "wb");
+    file = fopen("userdata.bin", "ab");
     if (file == NULL) {
         printf("파일을 열 수 없습니다.");
         return 1;
     }
 
+		while(1){
+			printf("1. 플레이어 정보 기록\n");
+			printf("2. 플레이어 정보 확인\n");
+			printf("3. 종료\n");
+			printf("입력하세요>");
+
+			scanf("%d",&menu);
+
+
+			if(menu == 1){
+				User u1;
+				int cnt = 0;
+				char* con;
+				
+				printf("ID를 입력하세요: ");
+				scanf("%s",u1.id);
+				printf("나이를 입력하세요: ");
+				scanf("%s",u1.age);
+				printf("HP를 입력하세요: ");
+				scanf("%s",u1.HP);
+				printf("X좌표를 입력하세요: ");
+				scanf("%lf",&u1.xp);
+				printf("Y좌표를 입력하세요: ");
+				scanf("%lf",&u1.yp);
+				while(1){
+					printf("동맹 ID를 입력하세요:");
+					scanf("%s",u1.friend[cnt++]);
+					printf("동맹을 계속 추가할까요?:");
+					scanf("%s",con);
+					if(!strcmp(con,"Y")) break;
+				}
+				break;
+			}
+			if(menu == 2){
+				printf("2\n");
+			}
+			if(menu == 3){
+				return 0;
+			}
+		}
     // 사용자로부터 입력 받기
-    printf("파일에 저장할 데이터를 입력하세요: ");
-    scanf("%s", input);
 
     // 파일에 데이터 쓰기
     fwrite(input, sizeof(char), sizeof(input), file);
